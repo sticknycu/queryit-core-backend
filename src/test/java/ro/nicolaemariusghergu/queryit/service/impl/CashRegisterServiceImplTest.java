9b61cd64fcbf78579abe2a71bbee69fb1b17f07a
nicugnm <nicolae.ghergu@s.unibuc.ro>
2021-12-08 15:43:37 +0200

    Mock tests

--- src/test/java/ro/nicolaemariusghergu/queryit/service/impl/CashRegisterServiceImplTest.java
+++ src/test/java/ro/nicolaemariusghergu/queryit/service/impl/CashRegisterServiceImplTest.java
@@ -1,12 +1,22 @@
 package ro.nicolaemariusghergu.queryit.service.impl;
 
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import ro.nicolaemariusghergu.queryit.exceptions.ResourceNotFoundException;
 import ro.nicolaemariusghergu.queryit.model.CashRegister;
 import ro.nicolaemariusghergu.queryit.model.data.TypePay;
 import ro.nicolaemariusghergu.queryit.model.data.TypeStatus;
+import ro.nicolaemariusghergu.queryit.repository.CashRegisterRepository;
 import ro.nicolaemariusghergu.queryit.service.CashRegisterService;
 
 import java.util.Optional;
@@ -14,22 +24,66 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CashRegisterServiceImplTest {
 
     private static final Long ID = 1L;
     private static final Integer NUMBER = 2;
 
     @Autowired
-    CashRegisterService cashRegisterService;
+    private CashRegisterService cashRegisterService;
 
-    @Test
-    void whenFindByIdthenReturnCasaMarcat() {
-        // given
+    @MockBean
+    private CashRegisterRepository cashRegisterRepository;
+
+    @TestConfiguration
+    class CashRegisterServiceImplTestContextConfiguration {
+
+        @Bean
+        public CashRegisterServiceImpl cashRegisterService() {
+            return new CashRegisterServiceImpl(cashRegisterRepository);
+        }
+    }
+
+    @BeforeAll
+    void configurateBeans() {
+        EasyRandom easyRandom = new EasyRandom(new EasyRandomParameters().collectionSizeRange(1, 2));
+
         CashRegister cashRegister = new CashRegister();
         cashRegister.setId(ID);
 
-        // when - Without Mock
-        Optional<CashRegister> found = cashRegisterService.findById(cashRegister.getId());
+        CashRegister cashRegisterRandomized = easyRandom.nextObject(CashRegister.class);
+
+        Mockito
+                .when(cashRegisterRepository.findById(ID))
+                .thenReturn(Optional.of(cashRegisterRandomized));
+
+        Mockito
+                .when(cashRegisterRepository.findByNumber(NUMBER))
+                .thenReturn(Optional.of(cashRegisterRandomized));
+
+        TypePay typePay = new TypePay();
+        typePay.setId(ID);
+
+        Mockito
+                .when(cashRegisterRepository.findCasaMarcatByTypePay(typePay))
+                .thenReturn(Optional.of(cashRegisterRandomized));
+
+        TypeStatus typeStatus = new TypeStatus();
+        typeStatus.setId(ID);
+
+        Mockito
+                .when(cashRegisterRepository.findCasaMarcatByTypeStatus(typeStatus))
+                .thenReturn(Optional.of(cashRegisterRandomized));
+
+        Mockito
+                .when(cashRegisterRepository.findCasaMarcatByTypeStatusAndTypePay(typeStatus, typePay))
+                .thenReturn(Optional.of(cashRegisterRandomized));
+    }
+
+    @Test
+    void whenFindByIdthenReturnCasaMarcat() {
+        Optional<CashRegister> found = cashRegisterService.findById(ID);
 
         // then
         found.ifPresentOrElse(casaMarcatFounded -> {
@@ -44,12 +98,8 @@ class CashRegisterServiceImplTest {
 
     @Test
     void whenFindByNumberthenReturnCasaMarcat() {
-        // given
-        CashRegister cashRegister = new CashRegister();
-        cashRegister.setNumber(NUMBER);
-
-        // when - Without Mock
-        Optional<CashRegister> found = cashRegisterService.findByNumber(cashRegister.getNumber());
+        // when
+        Optional<CashRegister> found = cashRegisterService.findByNumber(NUMBER);
 
         // then
         found.ifPresentOrElse(casaMarcatFounded -> {
@@ -70,13 +120,8 @@ class CashRegisterServiceImplTest {
         TypePay typePay = new TypePay();
         typePay.setId(ID);
 
-        // given
-        CashRegister cashRegister = new CashRegister();
-        cashRegister.setTypeStatus(typeStatus);
-        cashRegister.setTypePay(typePay);
-
-        // when - Without Mock
-        Optional<CashRegister> found = cashRegisterService.findCasaMarcatByTypeStatusAndTypePay(cashRegister.getTypeStatus(), typePay);
+        // when
+        Optional<CashRegister> found = cashRegisterService.findCasaMarcatByTypeStatusAndTypePay(typeStatus, typePay);
 
         // then
         found.ifPresentOrElse(casaMarcatFounded -> {
--- src/test/java/ro/nicolaemariusghergu/queryit/service/impl/TypeStatusServiceImplTest.java
+++ src/test/java/ro/nicolaemariusghergu/queryit/service/impl/TypeStatusServiceImplTest.java
@@ -1,17 +1,31 @@
 package ro.nicolaemariusghergu.queryit.service.impl;
 
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import ro.nicolaemariusghergu.queryit.exceptions.ResourceNotFoundException;
+import ro.nicolaemariusghergu.queryit.model.data.TypePay;
 import ro.nicolaemariusghergu.queryit.model.data.TypeStatus;
+import ro.nicolaemariusghergu.queryit.repository.data.TypePayRepository;
+import ro.nicolaemariusghergu.queryit.repository.data.TypeStatusRepository;
 import ro.nicolaemariusghergu.queryit.service.data.TypeStatusService;
+import ro.nicolaemariusghergu.queryit.service.impl.data.TypePayServiceImpl;
+import ro.nicolaemariusghergu.queryit.service.impl.data.TypeStatusServiceImpl;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TypeStatusServiceImplTest {
 
     private static final Long ID = 1L;
@@ -20,14 +34,41 @@ class TypeStatusServiceImplTest {
     @Autowired
     TypeStatusService typeStatusService;
 
-    @Test
-    void whenFindByIdthenReturnTypePay() {
-        // given
+    @MockBean
+    TypeStatusRepository typeStatusRepository;
+
+    @TestConfiguration
+    class TypeStatusServiceImplTestContextConfiguration {
+
+        @Bean
+        public TypeStatusServiceImpl typeStatusService() {
+            return new TypeStatusServiceImpl(typeStatusRepository);
+        }
+    }
+
+    @BeforeAll
+    void configurateBeans() {
+        EasyRandom easyRandom = new EasyRandom(new EasyRandomParameters().collectionSizeRange(1, 2));
+
         TypeStatus typeStatus = new TypeStatus();
         typeStatus.setId(ID);
 
-        // when - Without Mock
-        Optional<TypeStatus> found = typeStatusService.findById(typeStatus.getId());
+        TypeStatus typeStatusRandomized = easyRandom.nextObject(TypeStatus.class);
+
+        Mockito
+                .when(typeStatusRepository.findById(ID))
+                .thenReturn(Optional.of(typeStatusRandomized));
+
+        Mockito
+                .when(typeStatusRepository.findByType(TYPE))
+                .thenReturn(Optional.of(typeStatusRandomized));
+    }
+
+
+    @Test
+    void whenFindByIdthenReturnTypePay() {
+        // when
+        Optional<TypeStatus> found = typeStatusService.findById(ID);
 
         // then
         found.ifPresentOrElse(typePayFounded -> assertNotNull(typePayFounded.getType()), () -> {
@@ -37,12 +78,8 @@ class TypeStatusServiceImplTest {
 
     @Test
     void whenFindByTypethenReturnTypePay() {
-        // given
-        TypeStatus typeStatus = new TypeStatus();
-        typeStatus.setType(TYPE);
-
-        // when - Without Mock
-        Optional<TypeStatus> found = typeStatusService.findByType(typeStatus.getType());
+        // when
+        Optional<TypeStatus> found = typeStatusService.findByType(TYPE);
 
         // then
         found.ifPresentOrElse(typePayFounded -> assertNotNull(typePayFounded.getId()), () -> {
