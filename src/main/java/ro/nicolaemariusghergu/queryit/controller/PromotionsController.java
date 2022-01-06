69f254f22d99fc6b1df202d132efe4f5acd93f3d
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-01-06 22:55:35 +0200

    Update CrossOrigin port to Angular port.

--- src/main/java/ro/nicolaemariusghergu/queryit/controller/PromotionsController.java
+++ src/main/java/ro/nicolaemariusghergu/queryit/controller/PromotionsController.java
@@ -10,7 +10,7 @@ import ro.nicolaemariusghergu.queryit.service.PromotionService;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:60028")
 @Controller
 public record PromotionsController(PromotionService promotionService) {
 
