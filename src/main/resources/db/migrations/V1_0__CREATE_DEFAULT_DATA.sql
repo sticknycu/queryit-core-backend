1407661eb52482bb6e87b0c376811c031669d80a
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-01-09 11:47:47 +0200

    Flyway .sql files should not be changed.

--- src/main/resources/db/migrations/V1_0__CREATE_DEFAULT_DATA.sql
+++ src/main/resources/db/migrations/V1_0__CREATE_DEFAULT_DATA.sql
@@ -1,124 +1,68 @@
 CREATE TABLE IF NOT EXISTS
-    categories
-(
-    category_id
-    SERIAL
-    PRIMARY
-    KEY,
-    name
-    VARCHAR
-);
+    categories (
+                   category_id SERIAL,
+                   name VARCHAR,
+
+                   PRIMARY KEY (category_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    promotions
-(
-    promotion_id
-    SERIAL
-    PRIMARY
-    KEY,
-    name
-    VARCHAR,
-    description
-    VARCHAR
-);
+    promotions (
+                   promotion_id SERIAL,
+                   name VARCHAR,
+                   description VARCHAR,
+
+                   PRIMARY KEY (promotion_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    minimarkets
-(
-    minimarket_id
-    SERIAL
-    PRIMARY
-    KEY,
-    name
-    VARCHAR
-);
+    minimarkets (
+                    minimarket_id SERIAL,
+                    name VARCHAR,
+
+                    PRIMARY KEY (minimarket_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    deposits
-(
-    deposit_id
-    SERIAL
-    PRIMARY
-    KEY,
-    name
-    VARCHAR
-);
+    deposits (
+                 deposit_id SERIAL,
+                 name VARCHAR,
+
+                 PRIMARY KEY (deposit_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    trucks
-(
-    truck_id
-    SERIAL
-    PRIMARY
-    KEY,
-    serial_number
-    VARCHAR
-);
+    trucks (
+               truck_id SERIAL,
+               serial_number VARCHAR,
+
+               PRIMARY KEY (truck_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    manufacturers
-(
-    manufacturer_id
-    SERIAL
-    PRIMARY
-    KEY,
-    name
-    VARCHAR
-);
+    manufacturers (
+                      manufacturer_id SERIAL,
+                      name VARCHAR,
+
+                      PRIMARY KEY (manufacturer_id)
+    );
 
 CREATE TABLE IF NOT EXISTS
-    products
-(
-    product_id
-    SERIAL,
-    promotion_id
-    INTEGER,
-    category_id
-    INTEGER,
-    minimarket_id
-    INTEGER,
-    manufacturer_id
-    INTEGER,
-    name
-    VARCHAR,
-    price
-    FLOAT,
-    quantity
-    INTEGER,
-    icon_url
-    VARCHAR,
+    products (
+                 product_id SERIAL,
+                 promotion_id INTEGER,
+                 category_id INTEGER,
+                 minimarket_id INTEGER,
+                 manufacturer_id INTEGER,
+                 name VARCHAR,
+                 price FLOAT,
+                 quantity INTEGER,
+                 icon_url VARCHAR,
 
-    PRIMARY
-    KEY
-(
-    product_id
-),
-    CONSTRAINT fk_promotion_id FOREIGN KEY
-(
-    promotion_id
-) REFERENCES promotions
-(
-    promotion_id
-),
-    CONSTRAINT fk_category_id FOREIGN KEY
-(
-    category_id
-) REFERENCES categories
-(
-    category_id
-),
-    CONSTRAINT fk_minimarket_id FOREIGN KEY
-(
-    minimarket_id
-) REFERENCES minimarkets
-(
-    minimarket_id
-),
-    CONSTRAINT fk_manufacturer_id FOREIGN KEY
-(
-    manufacturer_id
-) REFERENCES manufacturers
-(
-    manufacturer_id
-)
+                 PRIMARY KEY (product_id),
+
+    CONSTRAINT fk_promotion_id FOREIGN KEY (promotion_id) REFERENCES promotions (promotion_id),
+    CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories (category_id),
+    CONSTRAINT fk_minimarket_id FOREIGN KEY (minimarket_id) REFERENCES minimarkets (minimarket_id),
+    CONSTRAINT fk_manufacturer_id FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id)
     );
