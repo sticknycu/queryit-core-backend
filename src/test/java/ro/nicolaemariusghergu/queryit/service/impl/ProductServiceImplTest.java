50d314edbd26129de1ff233e2bbc1fffe1d5479f
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-03-05 22:41:57 +0200

    Add WebClientConfig class

--- src/test/java/ro/nicolaemariusghergu/queryit/service/impl/ProductServiceImplTest.java
+++ src/test/java/ro/nicolaemariusghergu/queryit/service/impl/ProductServiceImplTest.java
@@ -3,31 +3,25 @@ package ro.nicolaemariusghergu.queryit.service.impl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.reactive.function.client.WebClient;
+import ro.nicolaemariusghergu.queryit.config.WebClientConfig;
 import ro.nicolaemariusghergu.queryit.model.Product;
-
-import static ro.nicolaemariusghergu.queryit.BackEndApplication.LOCALHOST;
 
 @SpringBootTest
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class ProductServiceImplTest {
+record ProductServiceImplTest(WebClientConfig webClientConfig) {
 
     private static final String PRODUCTS_URL = "/v1/products";
 
     private static final Long PRODUCT_ID = 1L;
 
-    @Autowired
-    WebClient.Builder webClientBuilder;
-
     @Test
     void whenGetDataThenShowResult() {
         // give
-        Product product = webClientBuilder
+        Product product = webClientConfig.getWebClientBuilder()
                 .build()
                 .get()
-                .uri(LOCALHOST + PRODUCTS_URL + "/" + PRODUCT_ID)
+                .uri(PRODUCTS_URL + "/" + PRODUCT_ID)
                 .retrieve()
                 .bodyToMono(Product.class)
                 .block();
